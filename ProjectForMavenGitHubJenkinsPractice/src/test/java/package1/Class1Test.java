88dedd33c668b7f703207106364e68dd3dbfd72a
Pavithra V <Pavithra V@Pavithra>
2025-12-03 17:31:46 +0530

    My first commit

---
+++ ProjectForMavenGitHubJenkinsPractice/src/test/java/package1/Class1Test.java
@@ -0,0 +1,18 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class1Test {
+	
+	
+	@Test
+	public void class1() {
+
+		Reporter.log("*************Hi Pavithran Sir************",true);
+		
+	}
+	
+	
+
+}
---
+++ ProjectForMavenGitHubJenkinsPractice/src/test/java/package1/Class3Test.java
@@ -0,0 +1,18 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class3Test {
+	
+	
+	@Test
+	public void class1() {
+
+		Reporter.log("*************Hi Pavithran Sir************",true);
+		
+	}
+	
+	
+
+}
