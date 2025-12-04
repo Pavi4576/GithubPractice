d01b765a549d20b58e64a32104a2ca98e89ee1c0
Pavithra V <Pavithra V@Pavithra>
2025-12-04 14:41:22 +0530

    my 3rd commit

---
+++ ProjectForMavenGitHubJenkinsPractice/src/test/java/package1/Class4Test.java
@@ -0,0 +1,19 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class4Test {
+	
+	
+	@Test
+	public void class1() {
+
+		Reporter.log("*************Hi Pavithran Sir************",true);
+		Reporter.log("*************Hi Pavithran Sir************",true);
+		
+	}
+	
+	
+
+}
